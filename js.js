8b0d63ef92bc908248ed040c570af39ba7d8d7f2
Jay Benaim <benaimjacob@gmail.com>
2019-06-08 14:21:35 -0400

    changed pictures

--- js.js
+++ js.js
@@ -2,8 +2,8 @@
   $('.box').on('click', function() {
       if (this.classList.contains("second")) { 
       $(this).toggleClass('secondH');
-      } else if (this.classList.contains( "dessert")) { 
-      $(this).toggleClass('dessertH'); 
+      } else if (this.classList.contains( "services")) { 
+      $(this).toggleClass('servicesH'); 
       } else if (this.classList.contains("first")) { 
       $(this).toggleClass('show-description');
       }; 
